b7e2e6b8f4c2cfc06f67b3cc04f84f1b83258dff
chakkun1121 <chakkun1121haruki@gmail.co>
2023-06-15 18:59:51 +0900

    バグ直し

--- app/edit/answer-hole.tsx
+++ app/edit/answer-hole.tsx
@@ -9,7 +9,7 @@ export default function AnswerHole({ answerXML }: { answerXML: Element }) {
     });
   return (
     <>
-      <p contentedita>
+      <p contenteditable>
         {answerArray.map((answer, i) => {
           return i % 2 === 0 ? answer : <Hole>{answer}</Hole>;
         })}
