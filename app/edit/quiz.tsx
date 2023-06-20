126a9b94d9c8fca0dbe108a9057bf25fc04096f4
chakkun1121 <chakkun1121haruki@gmail.co>
2023-06-21 08:25:16 +0900

    問題削除機能の実装

--- app/edit/quiz.tsx
+++ app/edit/quiz.tsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 import Answer from "./answer";
 import { resentFileArrayAtom } from "./main";
 import { useRecoilState } from "recoil";
-import { Input, Select } from "@chakra-ui/react";
+import { Input, Select, IconButton } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 export default function Quiz({
   index,
@@ -33,38 +34,54 @@ export default function Quiz({
       return newFileArray;
     });
   }
+  function deleteQuiz() {
+    setRecentFileArray((resentFileArray) => {
+      const newFileArray = [...resentFileArray];
+      newFileArray.splice(index, 1);
+      return newFileArray;
+    });
+  }
   return (
-    <div className="m-2 rounded bg-blue-300 p-2">
-      <div className="flex">
-        <div className="m-1 flex-1">
-          <Input
-            className="w-full"
-            id="problem"
-            placeholder="問題"
-            defaultValue={quizXML.getElementsByTagName("problem")[0]?.innerHTML}
-            onChange={saveChange}
-          />
-        </div>
-      </div>
-      <div className="md:flex">
-        <div className="m-1 flex-1">
-          <div className="flex-none">
-            <Answer index={index} type={QuizType} />
+    <div className="m-2 flex rounded bg-blue-300 p-2">
+      <div className="flex-1">
+        <div className="flex">
+          <div className="m-1 flex-1">
+            <Input
+              className="w-full"
+              id="problem"
+              placeholder="問題"
+              defaultValue={
+                quizXML.getElementsByTagName("problem")[0]?.innerHTML
+              }
+              onChange={saveChange}
+            />
           </div>
         </div>
-        <div className="m-1 flex-none md:w-80">
-          <Select
-            id="choose-type-select"
-            value={QuizType}
-            placeholder="形式を選択"
-            onChange={handleChange}
-          >
-            <option value={"standard"}>標準</option>
-            <option value={"hold"}>穴埋め形式</option>
-            <option value={"choices"}>選択問題</option>
-            <option value={"sorting"}>並べ替え問題</option>
-          </Select>
+        <div className="md:flex">
+          <div className="m-1 flex-1">
+            <div className="flex-none">
+              <Answer index={index} type={QuizType} />
+            </div>
+          </div>
+          <div className="m-1 flex-none md:w-80">
+            <Select
+              id="choose-type-select"
+              value={QuizType}
+              placeholder="形式を選択"
+              onChange={handleChange}
+            >
+              <option value={"standard"}>標準</option>
+              <option value={"hold"}>穴埋め形式</option>
+              <option value={"choices"}>選択問題</option>
+              <option value={"sorting"}>並べ替え問題</option>
+            </Select>
+          </div>
         </div>
+      </div>
+      <div className="flex-none">
+        <IconButton aria-label="問題を削除" onClick={deleteQuiz}>
+          <DeleteIcon />
+        </IconButton>
       </div>
     </div>
   );
