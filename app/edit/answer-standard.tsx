ede0182a17ee55c27b28adb18e26f9b882ac73d9
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-06-23 23:31:46 +0000

    #2 標準形式では修正完了

--- app/edit/answer-standard.tsx
+++ app/edit/answer-standard.tsx
@@ -1,16 +1,39 @@
 "use client";
 import { useState } from "react";
-import { getAnswerXML } from "./main";
+import { getAnswerXML, resentFileArrayAtom } from "./main";
 import { Input } from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
 
 export default function AnswerStandard({ index }: { index: number }) {
-  const [answerXML, setAnswerXML] = useState<Element>(getAnswerXML(index));
+  const [answerXML] = useState<Element>(getAnswerXML(index));
+  const [recentFileArray, setRecentFileArray] =
+    useRecoilState<Array<Element>>(resentFileArrayAtom);
   return (
     <Input
       className="w-full"
       id="answer"
       placeholder="答え"
       defaultValue={answerXML?.innerHTML}
+      // onchangeを追加する
+      onChange={(e) => {
+        setRecentFileArray(() => {
+          const answerXML: Element = new DOMParser().parseFromString(
+            `<answer>${e.target.value}</answer>`,
+            "text/xml"
+          ).documentElement;
+          const cashRecentFileArray = [...recentFileArray];
+          return cashRecentFileArray.map((quizXML: Element, i: number) => {
+            if (index == i) {
+              const cashQuizXML = quizXML.cloneNode(true) as Element;
+              cashQuizXML.getElementsByTagName("answer")[0].innerHTML =
+                answerXML.innerHTML;
+              return cashQuizXML;
+            } else {
+              return quizXML;
+            }
+          });
+        });
+      }}
     />
   );
 }
