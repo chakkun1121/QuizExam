e9f2889899f3c963f4ba2f022e6b7df10bf2063d
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-06-25 23:08:11 +0000

    fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,12 @@
 import Link from "next/link";
-import Header from "./header";
-import Footer from "./footer";
 export const metadata = {
   title: "404 ページが見つかりません | QuizExam | chakkun1121",
 };
 export default function NotFound() {
   return (
     <>
-      <Header />
-      <main>
         <h1>404 ページが見つかりません</h1>
         <Link href="/">ホームへ戻る</Link>
-      </main>
-      <Footer />
     </>
   );
 }
