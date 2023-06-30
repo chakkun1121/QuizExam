060305c082d83467a3389881d8ce43f2771e6085
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-06-30 09:23:36 +0000

    footerにホームページへのリンクを追加

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "./_components/link";
 
 export default function Footer() {
   return (
@@ -6,7 +6,9 @@ export default function Footer() {
       <footer className="flex justify-center bg-purple-800 p-2 text-white">
         <div className="flex-none">
           <div className="">
-            <p>© 2023 chakkun1121</p>
+            <Link href="https://chakkun1121.github.io/" target="_blank">
+              © 2023 chakkun1121
+            </Link>
           </div>
           <div className="">
             <Link href="/privacy-policy" className="text-white">
