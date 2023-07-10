a912a827319057f0b162f36b6cdf284de43c32c5
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-07-10 04:07:51 +0000

    ミス訂正

--- lib/server/sync.ts
+++ lib/server/sync.ts
@@ -2,6 +2,6 @@ import { filesInfoType } from "../../types/filesInfoType";
 
 export function syncFilesInfo(localFilesInfo: filesInfoType) {
   return new Promise<string>((res) => {
-    res("ファイルを同機しました(テスト)");
+    res("ファイルを同期しました(テスト)");
   }); //test
 }
